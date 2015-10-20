afb7d9214b69e5bbd021b93b67b668b11dd617a5
Sachi Jain <sachijai@buffalo.edu>
2015-10-20 18:01:12 -0500

    Update Mapper1.java

--- src/Mapper1.java
+++ src/Mapper1.java
@@ -21,37 +21,37 @@ public class Mapper1 extends Mapper<LongWritable, Text, Text, Text>
 	
 	public void map(LongWritable key, Text value, Context context)
 	{
-		 FileSplit fileSplit = (FileSplit)context.getInputSplit();
-		 String fileName = fileSplit.getPath().getName();
-		 String day="";
-	     String temp[]=new String[7];
-		 String date[]=new String[3];
-		 String outputKey="";
-		 String line = value.toString();
-	     temp=line.split("\\,");
+		FileSplit fileSplit = (FileSplit)context.getInputSplit();
+		String fileName = fileSplit.getPath().getName();
+		String day="";
+	     	String temp[]=new String[7];
+		String date[]=new String[3];
+		String outputKey="";
+		String line = value.toString();
+        	temp=line.split("\\,");
 	     
 	     
-	     if(temp[0].contains("-"))
-	     { 
-	    	 date= temp[0].split("\\-");
-	    	 outputKey=fileName+"____"+date[0]+","+date[1];
-	    	 day=date[2]+"__"+temp[6];
-	    	 Text op= new Text(outputKey);
-	    	 Text va= new Text(day);
-	    	 try 
-	    	 {
-	    		 context.write(op,va);
-	    	 } 
-	    	 catch (IOException e) 
-	    	 {
+		if(temp[0].contains("-"))
+	     	{ 
+	    		date= temp[0].split("\\-");
+	    		outputKey=fileName+"____"+date[0]+","+date[1];
+	    	 	day=date[2]+"__"+temp[6];
+	    	 	Text op= new Text(outputKey);
+	    	 	Text va= new Text(day);
+	    	 	try 
+	    	 	{
+	    			context.write(op,va);
+	    	 	} 
+	    	 	catch (IOException e) 
+	    	 	{
 	    		 // TODO Auto-generated catch block
-	    		 e.printStackTrace();
-	    	 } 
-	    	 catch (InterruptedException e) 
-	    	 {
+	    			 e.printStackTrace();
+	    	 	} 
+	    	 	catch (InterruptedException e) 
+	    	 	{
 	    		 // TODO Auto-generated catch block
-	    		 e.printStackTrace();
-	    	 }
+	    			 e.printStackTrace();
+	    	 	}
 	      }
 	}
 }
