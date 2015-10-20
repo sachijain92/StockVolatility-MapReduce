3be21fdcd890e3017f88a7e73ec29eb23d4150f7
Sachi Jain <sachijai@buffalo.edu>
2015-10-20 18:07:29 -0500

    Update StockVolatilityDriver.java

--- src/StockVolatilityDriver.java
+++ src/StockVolatilityDriver.java
@@ -25,60 +25,60 @@ public class StockVolatilityDriver
 		try 
 		{
 				// Create a new Job
-				long start = new Date().getTime();
-				Job job = Job.getInstance();
-				Job job2=Job.getInstance();
-		     	Job job3=Job.getInstance();
+			long start = new Date().getTime();
+			Job job = Job.getInstance();
+			Job job2=Job.getInstance();
+	     		Job job3=Job.getInstance();
 		     
-			    job.setJarByClass(StockVolatilityDriver.class);
-			    job2.setJarByClass(StockVolatilityDriver.class);
-			    job3.setJarByClass(StockVolatilityDriver.class);
+			job.setJarByClass(StockVolatilityDriver.class);
+			job2.setJarByClass(StockVolatilityDriver.class);
+			job3.setJarByClass(StockVolatilityDriver.class);
 			
-				job.setMapperClass(Mapper1.class);		
-				job.setReducerClass(Reducer1.class);
-				job2.setMapperClass(Mapper2.class);
-				job2.setReducerClass(Reducer2.class);
-				job3.setMapperClass(Mapper3.class);
-				job3.setReducerClass(Reducer3.class);
+			job.setMapperClass(Mapper1.class);		
+			job.setReducerClass(Reducer1.class);
+			job2.setMapperClass(Mapper2.class);
+			job2.setReducerClass(Reducer2.class);
+			job3.setMapperClass(Mapper3.class);
+			job3.setReducerClass(Reducer3.class);
 			
-				job.setMapOutputKeyClass(Text.class);
-				job.setMapOutputValueClass(Text.class);
-				job.setOutputKeyClass(Text.class);
-				job.setOutputValueClass(DoubleWritable.class);
-				job2.setMapOutputKeyClass(Text.class);
-				job2.setMapOutputValueClass(DoubleWritable.class);
-				job2.setOutputKeyClass(Text.class);
-				job2.setOutputValueClass(DoubleWritable.class);
-				job3.setMapOutputKeyClass(IntWritable.class);
-				job3.setMapOutputValueClass(Text.class);
-				job3.setOutputKeyClass(Text.class);
-				job3.setOutputValueClass(DoubleWritable.class);
+			job.setMapOutputKeyClass(Text.class);
+			job.setMapOutputValueClass(Text.class);
+			job.setOutputKeyClass(Text.class);
+			job.setOutputValueClass(DoubleWritable.class);
+			job2.setMapOutputKeyClass(Text.class);
+			job2.setMapOutputValueClass(DoubleWritable.class);
+			job2.setOutputKeyClass(Text.class);
+			job2.setOutputValueClass(DoubleWritable.class);
+			job3.setMapOutputKeyClass(IntWritable.class);
+			job3.setMapOutputValueClass(Text.class);
+			job3.setOutputKeyClass(Text.class);
+			job3.setOutputValueClass(DoubleWritable.class);
 			
 			
 			
-				job.setInputFormatClass(TextInputFormat.class);
-				job.setOutputFormatClass(TextOutputFormat.class);
-				job2.setInputFormatClass(TextInputFormat.class);
-				job2.setOutputFormatClass(TextOutputFormat.class);
-				job3.setInputFormatClass(TextInputFormat.class);
-				job3.setOutputFormatClass(TextOutputFormat.class);
+			job.setInputFormatClass(TextInputFormat.class);
+			job.setOutputFormatClass(TextOutputFormat.class);
+			job2.setInputFormatClass(TextInputFormat.class);
+			job2.setOutputFormatClass(TextOutputFormat.class);
+			job3.setInputFormatClass(TextInputFormat.class);
+			job3.setOutputFormatClass(TextOutputFormat.class);
 			
-				FileInputFormat.addInputPath(job, new Path(args[0]));
-				FileOutputFormat.setOutputPath(job, new Path(args[1]+"hey"));
-				FileInputFormat.addInputPath(job2, new Path(args[1]+"hey"));
-				FileOutputFormat.setOutputPath(job2, new Path(args[1]+"output"));
-				FileInputFormat.addInputPath(job3, new Path(args[1]+"output"));
-				FileOutputFormat.setOutputPath(job3, new Path(args[1]));
+			FileInputFormat.addInputPath(job, new Path(args[0]));
+			FileOutputFormat.setOutputPath(job, new Path(args[1]+"hey"));
+			FileInputFormat.addInputPath(job2, new Path(args[1]+"hey"));
+			FileOutputFormat.setOutputPath(job2, new Path(args[1]+"output"));
+			FileInputFormat.addInputPath(job3, new Path(args[1]+"output"));
+			FileOutputFormat.setOutputPath(job3, new Path(args[1]));
 			
-				job.setJarByClass(StockVolatilityDriver.class);
-				job.waitForCompletion(true);		
-				job2.waitForCompletion(true);
-				boolean status=job3.waitForCompletion(true);
-				if (status == true) 
-				{
-					long end = new Date().getTime();
-					System.out.println("\nJob took " + (end-start)/60000 + " minutes\n");
-				}
+			job.setJarByClass(StockVolatilityDriver.class);
+			job.waitForCompletion(true);		
+			job2.waitForCompletion(true);
+			boolean status=job3.waitForCompletion(true);
+			if (status == true) 
+			{
+				long end = new Date().getTime();
+				System.out.println("\nJob took " + (end-start)/60000 + " minutes\n");
+			}
 			
 			
 			
